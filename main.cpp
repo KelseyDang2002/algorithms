fa82bccfd1329b85dc30caa0f0615440bd2f88c3
KelseyDang2002 <kelseydang813@gmail.com>
2024-06-22 14:24:24 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 #include <cstdlib>
+#include <functional>
 #include <chrono>
 
-const int ARR_SIZE = 1000; // size of array
+const int ARR_SIZE = 5000; // size of array
+
+class Array {
+    public:
+        int array[ARR_SIZE];
+
+        void generate_arr();
+        void print_arr(std::string text);
+        void bubble_sort();
+        void bubble_sort_opt();
+        void selection_sort();
+        void merge_sort();
+        void quick_sort();
+};
 
 // Function to generate array 
-void generate_arr(int array[], int size) {
+void Array::generate_arr() {
     srand(time(0)); // seed
 
-    for (int i = 0; i < size; i++) {
-        array[i] = (rand() % (10000 + 1)); // random integer between 0 and 10000
+    for (int i = 0; i < ARR_SIZE; i++) {
+        array[i] = (rand() % (100000 + 1)); // random integer between 0 and 100000
     }
 }
 
 // Function to print array
-void print_arr(int array[], int size, std::string text) {
+void Array::print_arr(std::string text) {
     std::cout << "\n" << text << "\n";
     std::cout << "------------------------------------------------------------------------------------\n";
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < ARR_SIZE; i++) {
         std::cout << "\t" << array[i];
 
         if ((i + 1) % 10 == 0) {
@@ -34,14 +48,14 @@ void swap(int *x, int *y) {
 }
 
 // Function to do Bubble Sort
-void bubble_sort(int array[], int size, std::string text) {
-    std::cout << "\n" << text << "\n";
+void Array::bubble_sort() {
+    std::cout << "\nBubble Sort\n";
     
     bool swapped;
-    for (int i = 0; i < (size - 1); i++) {
+    for (int i = 0; i < (ARR_SIZE - 1); i++) {
         swapped = false;
 
-        for (int j = 0; j < (size - i - 1); j++) {
+        for (int j = 0; j < (ARR_SIZE - i - 1); j++) {
             // swap if low element in pair > second element in pair
             if (array[j] > array[j + 1]) {
                 swap(&array[j], &array[j + 1]);
@@ -57,12 +71,12 @@ void bubble_sort(int array[], int size, std::string text) {
 }
 
 // Function to do Optimized Bubble Sort
-void bubble_sort_opt(int array[], int size, std::string text) {
-    std::cout << "\n" << text << "\n";
+void Array::bubble_sort_opt() {
+    std::cout << "\nOptimized Bubble Sort\n";
     
     bool swapped = false;
-    for (int i = 1; i <= size; i++) {
-        if (i == size) {
+    for (int i = 1; i <= ARR_SIZE; i++) {
+        if (i == ARR_SIZE) {
             if (!swapped) {
                 break;
             }
@@ -79,13 +93,13 @@ void bubble_sort_opt(int array[], int size, std::string text) {
 }
 
 // Function to do Selection Sort
-void selection_sort(int array[], int size, std::string text) {
-    std::cout << "\n" << text << "\n";
+void Array::selection_sort() {
+    std::cout << "\nSelection Sort\n";
 
-    for (int i = 0; i < size - 1; i++) {
+    for (int i = 0; i < ARR_SIZE - 1; i++) {
         int min_value = i;
 
-        for (int j = i + 1; j < size; j++) {
+        for (int j = i + 1; j < ARR_SIZE; j++) {
             if (array[j] < array[min_value]) {
                 min_value = j;
             }
@@ -142,10 +156,10 @@ void merge_sort_recursion(int array[], int left, int right) {
 }
 
 // Function to do Merge Sort
-void merge_sort(int array[], int size, std::string text) {
-    std::cout << "\n" << text << "\n";
+void Array::merge_sort() {
+    std::cout << "\nMerge Sort\n";
 
-    merge_sort_recursion(array, 0, size - 1);
+    merge_sort_recursion(array, 0, ARR_SIZE - 1);
 }
 
 // Function that separates values less than and greater than the pivot in Quick Sort
@@ -184,50 +198,56 @@ void quick_sort_recursion(int array[], int low, int high) {
 }
 
 // Function to do Quick Sort
-void quick_sort(int array[], int size, std::string text) {
-    std::cout << "\n" << text << "\n";
+void Array::quick_sort() {
+    std::cout << "\nQuick Sort\n";
 
     srand(time(NULL));
-    quick_sort_recursion(array, 0, size - 1);
+    quick_sort_recursion(array, 0, ARR_SIZE - 1);
 }
 
 // Main
 int main() {
-    int array[ARR_SIZE]; // array
-    int array2[ARR_SIZE];
-    int array3[ARR_SIZE];
-    int array4[ARR_SIZE];
-    int array5[ARR_SIZE];
+    Array array1, array2, array3, array4, array5;
 
-    // generate_arr(array, ARR_SIZE);
-    // generate_arr(array2, ARR_SIZE);
-    // generate_arr(array3, ARR_SIZE);
-    // generate_arr(array4, ARR_SIZE);
-    generate_arr(array5, ARR_SIZE);
-
-    // print_arr(array, ARR_SIZE, "Unsorted Array:");
-    // print_arr(array2, ARR_SIZE, "Unsorted Array2:");
-    // print_arr(array3, ARR_SIZE, "Unsorted Array3:");
-    // print_arr(array4, ARR_SIZE, "Unsorted Array4:");
-    print_arr(array5, ARR_SIZE, "Unsorted Array5:");
+    array1.generate_arr();
+    array2.generate_arr();
+    array3.generate_arr();
+    array4.generate_arr();
+    array5.generate_arr();
+    
+    array1.print_arr("Unsorted Array:");
 
     auto start = std::chrono::high_resolution_clock::now(); // time start
-
-    // bubble_sort(array, ARR_SIZE, "Bubble Sort");
-    // bubble_sort_opt(array2, ARR_SIZE, "Optimized Bubble Sort");
-    // selection_sort(array3, ARR_SIZE, "Selection Sort");
-    // merge_sort(array4, ARR_SIZE, "Merge Sort");
-    quick_sort(array5, ARR_SIZE, "Quick Sort");
-
+    array1.bubble_sort();
     auto stop = std::chrono::high_resolution_clock::now(); // time stop
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); // time
     std::cout << "Execution time: " << duration.count() << " microseconds\n"; // print time
 
-    // print_arr(array, ARR_SIZE, "Sorted Array:");
-    // print_arr(array2, ARR_SIZE, "Sorted Array2:");
-    // print_arr(array3, ARR_SIZE, "Sorted Array3:");
-    // print_arr(array4, ARR_SIZE, "Sorted Array4:");
-    print_arr(array5, ARR_SIZE, "Sorted Array5:");
+    // auto start2 = std::chrono::high_resolution_clock::now(); // time start
+    // array2.bubble_sort_opt();
+    // auto stop2 = std::chrono::high_resolution_clock::now(); // time stop
+    // auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop2 - start2); // time
+    // std::cout << "Execution time: " << duration2.count() << " microseconds\n"; // print time
+
+    // auto start3 = std::chrono::high_resolution_clock::now(); // time start
+    // array3.selection_sort();
+    // auto stop3 = std::chrono::high_resolution_clock::now(); // time stop
+    // auto duration3 = std::chrono::duration_cast<std::chrono::microseconds>(stop3 - start3); // time
+    // std::cout << "Execution time: " << duration3.count() << " microseconds\n"; // print time
+
+    // auto start4 = std::chrono::high_resolution_clock::now(); // time start
+    // array4.merge_sort();
+    // auto stop4 = std::chrono::high_resolution_clock::now(); // time stop
+    // auto duration4 = std::chrono::duration_cast<std::chrono::microseconds>(stop4 - start4); // time
+    // std::cout << "Execution time: " << duration3.count() << " microseconds\n"; // print time
+
+    // auto start5 = std::chrono::high_resolution_clock::now(); // time start
+    // array5.quick_sort();
+    // auto stop5 = std::chrono::high_resolution_clock::now(); // time stop
+    // auto duration5 = std::chrono::duration_cast<std::chrono::microseconds>(stop5 - start5); // time
+    // std::cout << "Execution time: " << duration5.count() << " microseconds\n"; // print time
+    
+    array1.print_arr("Sorted Array:");
 
     return 0;
 }
